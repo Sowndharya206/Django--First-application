3cfb5eab091b16fbad5ac4d33fa7f5a9a8a156cc
Sowndharya206 <saidharanisai143@gmail.com>
2021-01-26 21:17:26 +0530

    commit

--- myapp/models.py
+++ myapp/models.py
@@ -8,4 +8,11 @@ class workers(models.Model):
     entrydate = models.DateField()  
     age = models.IntegerField() 
     contact = models.IntegerField()   
-   
+from __future__ import unicode_literals  
+from django.db import models  
+  
+class Student(models.Model):  
+    first_name = models.CharField(max_length=20)  
+    last_name  = models.CharField(max_length=30)  
+    class Meta:  
+        db_table = "student"    
